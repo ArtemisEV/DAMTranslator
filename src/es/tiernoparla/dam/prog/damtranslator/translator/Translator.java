26cf4cdc50e75e4ece7acc65dd6e38a106eaae11
Reyes <diego.blazquezreyes@tiernoparla.es>
2023-04-01 01:20:27 +0200

    V1-Byte v1

--- src/es/tiernoparla/dam/prog/damtranslator/translator/Translator.java
+++ src/es/tiernoparla/dam/prog/damtranslator/translator/Translator.java
@@ -1,12 +1,10 @@
 package es.tiernoparla.dam.prog.damtranslator.translator;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import es.tiernoparla.dam.prog.damtranslator.dictionary.Dictionary;
 
@@ -24,41 +22,37 @@ public class Translator {
 
     public void translate(String inputFilePath, String outputFilePath) throws IOException {
         try {
-
             File originalFile = new File(inputFilePath);
             File parentDirectory = originalFile.getParentFile();
 
             String translatedFileDirectory = parentDirectory.getAbsolutePath() + "/" + PREFIX + originalFile.getName();
             File translatedFile = new File(translatedFileDirectory);
 
-            BufferedReader reader = new BufferedReader(new FileReader(originalFile));
+            FileInputStream fis = new FileInputStream(originalFile);
+            byte[] data = new byte[(int) originalFile.length()];
+            fis.read(data);
+            fis.close();
+
+            String content = new String(data, "UTF-8");
+
+            String[] words = content.split("\\s+");
+
             BufferedWriter writer = new BufferedWriter(new FileWriter(translatedFile));
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Split the line into words using a StringTokenizer
-                StringTokenizer tokenizer = new StringTokenizer(line, " ");
-                while (tokenizer.hasMoreTokens()) {
-                    // Get the next word
-                    String word = tokenizer.nextToken();
+            for (String word : words) {
+                // Translate the word using the dictionary
+                String translatedWord = dictionary.translate(word);
 
-                    // Translate the word using the dictionary
-                    String translatedWord = dictionary.translate(word);
-
-                    // Write the translated word to the output file
-                    writer.write(translatedWord + " ");
-                }
-
-                // Write a newline character to separate lines
-                writer.newLine();
+                // Write the translated word to the output file
+                writer.write(translatedWord + " ");
             }
 
-            reader.close();
             writer.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     public static String getPrefix() {
