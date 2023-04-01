a87e3f9ef681ef3dfa265386dae2f62261982284
Reyes <bimseqq.br@gmail.com>
2023-04-01 02:09:59 +0200

    V2 - Comments

--- src/es/tiernoparla/dam/prog/damtranslator/dictionary/DictionaryLoader.java
+++ src/es/tiernoparla/dam/prog/damtranslator/dictionary/DictionaryLoader.java
@@ -7,16 +7,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DictionaryLoader {
-    
+
     public static Map<String, String> loadDictionary(String filePath, String separator) throws IOException {
+        // Create an empty map to store the dictionary
         Map<String, String> dictionary = new HashMap<>();
+
+        // Create a BufferedReader to read the text file
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        // Read each line of the file
         String line;
         while ((line = reader.readLine()) != null) {
+            // Split the line into words using the specified separator
             String[] words = line.split(separator);
+
+            // Add the word and its translation to the dictionary map
             dictionary.put(words[0], words[1]);
         }
+
+        // Close the reader
         reader.close();
+
+        // Return the dictionary map
         return dictionary;
     }
 }
