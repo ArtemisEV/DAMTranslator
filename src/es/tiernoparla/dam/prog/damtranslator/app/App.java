f56ec69ac7331d2a2483e7fb36a5d35512d35034
ReyesEV <diego.blazquezreyes@tiernoparla.es>
2023-05-11 12:38:14 +0200

    V3 - Last Name

--- src/es/tiernoparla/dam/prog/damtranslator/app/App.java
+++ src/es/tiernoparla/dam/prog/damtranslator/app/App.java
@@ -7,10 +7,12 @@ import es.tiernoparla.dam.prog.damtranslator.translator.Translator;
 
 public class App {
 
-    public static final String MSG_FILE_NAME = "Insert the name of the file you want to translate (The file needs to be in: src/es/tiernoparla/dam/prog/damtranslator/files): ";
-    public static final String FOLDER_PATH_ES = "src/es/tiernoparla/dam/prog/damtranslator/files/EsToEng";
-    public static final String FOLDER_PATH_ENG = "src/es/tiernoparla/dam/prog/damtranslator/files/EngToEs";
-    public static final String DICTIONARY_PATH = "src/es/tiernoparla/dam/prog/damtranslator/files/dictionary/dictionary.txt";
+    public static final String MSG_FILE_NAME_ES = "Insert the name of the file you want to translate (The file needs to be in: src/es/tiernoparla/dam/prog/damtranslator/files/EsToEng/): ";
+    public static final String MSG_FILE_NAME_ENG = "Insert the name of the file you want to translate (The file needs to be in: src/es/tiernoparla/dam/prog/damtranslator/files/EngToEs): ";
+    public static final String FOLDER_PATH_ES = "src/es/tiernoparla/dam/prog/damtranslator/files/EsToEng/";
+    public static final String FOLDER_PATH_ENG = "src/es/tiernoparla/dam/prog/damtranslator/files/EngToEs/";
+    public static final String DICTIONARY_PATH_ES = "src/es/tiernoparla/dam/prog/damtranslator/files/dictionary/dictionary.txt";
+    public static final String DICTIONARY_PATH_ENG = "src/es/tiernoparla/dam/prog/damtranslator/files/dictionary/engdictionary.txt";
     public static final String DICTIONARY_SEPARATOR = ",";
     public static final String MSG_ASK = "Choose an option:";
     public static final String MSG_ES = "(1.) Translate Spanish -> English";
@@ -21,21 +23,20 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        Dictionary dictionaryEs = new Dictionary(DICTIONARY_PATH, DICTIONARY_SEPARATOR);
-        Dictionary dictionaryEng = new Dictionary(DICTIONARY_PATH, DICTIONARY_SEPARATOR);
+        Dictionary dictionaryEs = new Dictionary(DICTIONARY_PATH_ES, DICTIONARY_SEPARATOR);
+        Dictionary dictionaryEng = new Dictionary(DICTIONARY_PATH_ENG, DICTIONARY_SEPARATOR);
         Translator translatorEs = new Translator(dictionaryEs);
         Translator translatorEng = new Translator(dictionaryEng);
-
-        int choice = 0;
-        choice = sc.nextInt();
 
         System.out.println(MSG_ASK);
         System.out.println(MSG_ES);
         System.out.println(MSG_ENG);
         System.out.print(OPTION);
+        int choice = 0;
+        choice = sc.nextInt();
         switch (choice) {
             case 1:
-                System.out.print(MSG_FILE_NAME);
+                System.out.print(MSG_FILE_NAME_ES);
                 Scanner sc1 = new Scanner(System.in);
                 String fileNameEs = sc1.nextLine();
                 String filePathEs = FOLDER_PATH_ES + fileNameEs;
@@ -44,7 +45,7 @@ public class App {
                 break;
 
             case 2:
-                System.out.print(MSG_FILE_NAME);
+                System.out.print(MSG_FILE_NAME_ENG);
                 Scanner sc2 = new Scanner(System.in);
                 String fileNameEng = sc2.nextLine();
                 String filePathEng = FOLDER_PATH_ENG + fileNameEng;
--- src/es/tiernoparla/dam/prog/damtranslator/dictionary/Dictionary.java
+++ src/es/tiernoparla/dam/prog/damtranslator/dictionary/Dictionary.java
@@ -5,15 +5,6 @@ import java.util.Map;
 
 public class Dictionary {
 
-    public static final String ES_WORD_1 = "hola";
-    public static final String WORD_1 = "hello";
-    public static final String ES_WORD_2 = "mi";
-    public static final String WORD_2 = "my";
-    public static final String ES_WORD_3 = "apellido";
-    public static final String WORD_3 = "last name";
-    public static final String ES_WORD_4 = "es";
-    public static final String WORD_4 = "is";
-
     private Map<String, String> dictionary;
 
     public Dictionary(String filePath, String separator) throws IOException {
@@ -23,38 +14,6 @@ public class Dictionary {
     // Translate the given word to its corresponding translated word, or return the word itself if not found in the dictionary
     public String translate(String word) {
         return dictionary.getOrDefault(word.toLowerCase(), word);
-    }
-
-    public static String getEsWord1() {
-        return ES_WORD_1;
-    }
-
-    public static String getWord1() {
-        return WORD_1;
-    }
-
-    public static String getEsWord2() {
-        return ES_WORD_2;
-    }
-
-    public static String getWord2() {
-        return WORD_2;
-    }
-
-    public static String getEsWord3() {
-        return ES_WORD_3;
-    }
-
-    public static String getWord3() {
-        return WORD_3;
-    }
-
-    public static String getEsWord4() {
-        return ES_WORD_4;
-    }
-
-    public static String getWord4() {
-        return WORD_4;
     }
 
     public Map<String, String> getDictionary() {
